e855b9903abc05d5accf262c771b507381e52730
Max <max.belenchuk@gmail.com>
2023-04-19 00:02:55 -0300

    Watched class videos to figure out certain things. Should be functioning. User schema defined

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            max_length: 50,
+            trim: true,
+            
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+\@.+\..+/, 'Please make sure email matches']
+        },
+        thoughts: [
+            {
+              type: Schema.Types.ObjectId,
+              ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+              type: Schema.Types.ObjectId,
+              ref: 'User',
+            },
+        ],
+    },
+        {
+            toJSON: {
+              virtuals: true,  
+            },
+            id: false,
+        },
+);
+
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.legnth;
+});
+
+const User = model('user', userSchema);
+
+
+module.exports = User;
+
