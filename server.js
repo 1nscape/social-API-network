0faa4ff883e6aa2451f7733f592b38f63e00d472
1nscape <115809175+1nscape@users.noreply.github.com>
2023-04-20 12:23:05 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const db = require('./config/connection');
+const routes = require('./routes');
 
-const app = express(); 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
+const app = express();
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(epxress.urlencoded({exnteded: true}));
-app.use(express.static('public'));
-app.use(require('/'))
+app.use(routes);
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`App running on port ${PORT}!`);
+    });
+});
